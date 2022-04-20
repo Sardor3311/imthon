995fa7ad604f7c95169bc86fe1e8bf5085d4cd20
sardor3311 <erik331121@gmail.com>
2022-04-20 07:50:32 +0500

    first commit

---
+++ src/main/java/imtixon/uz/start_up/StartUpApplication.java
@@ -0,0 +1,13 @@
+package imtixon.uz.start_up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imtixon/uz/start_up/StartUpApplicationTests.java
@@ -0,0 +1,13 @@
+package imtixon.uz.start_up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
